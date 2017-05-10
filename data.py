ef2fde310ee93712b721ebe76f872a1c4d418e61
James Ye <jamye506@gmail.com>
2017-05-10 12:40:21 -0700

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'Body': 'Insert al the text for the body text here. This is the dummy data for the file right now,',
+            'author': 'James Ye',
+            'create date': '04-45-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'Body': 'IBlafkjwefoiwehfoiwejfoijweifjweoifjoiwejfiewt here. This is the dummy data for the file right now,',
+            'author': 'Chloe Tarng',
+            'create date': '04-45-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'Body': 'All I know is Run, Run, Run, Run, Run, Run, Run, Run, Run, Run,  dummy data for the file right now,',
+            'author': 'Penelo Chen',
+            'create date': '04-45-2017'
+        }
+    ]
+    return articles
